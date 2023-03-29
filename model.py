d6ab607ce6be8937be9c8183d1f2eb2449e40b58
Christian Rodriguez <crodriguezochoa@ucdavis.edu>
2023-03-29 13:56:53 -0700

    made script to seed database and defined functions to create user, movie, and rating

--- model.py
+++ model.py
@@ -50,7 +50,7 @@ class Rating(db.Model):
     user = db.relationship("User", back_populates="ratings")
 
     def __repr__(self):
-        return f'<Rating rating_id={self.rating_id} score={self.score}'
+        return f'<Rating rating_id={self.rating_id} score={self.score}>'
 
 def connect_to_db(flask_app, db_uri="postgresql:///ratings", echo=True):
     flask_app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
