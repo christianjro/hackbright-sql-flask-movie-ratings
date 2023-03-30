ae873962103411c63bb840229308f95ee86a0a7c
Christian Rodriguez <crodriguezochoa@ucdavis.edu>
2023-03-30 12:18:04 -0700

    created server routes to render database data

--- crud.py
+++ crud.py
@@ -2,6 +2,7 @@
 
 from model import db, User, Movie, Rating, connect_to_db
 
+# User Functions
 def create_user(email, password):
     """Create and return a new user."""
 
@@ -9,6 +10,16 @@ def create_user(email, password):
 
     return user
 
+def get_all_users():
+    """Return all users."""
+    return User.query.all()
+
+def get_user_by_id(user_id):
+    """Return a user by primary key."""
+    return User.query.get(user_id)
+
+
+# Movie Functions
 def create_movie(title, overview, release_date, poster_path):
     """Create and return a new movie."""
 
@@ -20,12 +31,26 @@ def create_movie(title, overview, release_date, poster_path):
     
     return movie
 
+def get_all_movies():
+    """Return all movies."""
+    return Movie.query.all()
+
+def get_movie_by_id(movie_id):
+    """Return a movie by primary key."""
+    return Movie.query.get(movie_id)
+
+
+# Rating Functions
 def create_rating(user, movie, score):
     """Create and return a new rating."""
 
     rating = Rating(user=user, movie=movie, score=score)
 
     return rating
+
+def get_ratings_by_user(user):
+    """Return ratings user has made."""
+    return Rating.query.filter_by(user=user).all()
 
 
 if __name__ == '__main__':
